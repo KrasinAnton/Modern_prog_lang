61a00842742909eefeec5fe6976225ee6908dc48
KrasinAnt <Anton.Kartavchenko@urfu.me>
2024-04-23 21:17:52 +0500

    commit

--- Lab's/Lb_6/stroka.cpp
+++ Lab's/Lb_6/stroka.cpp
@@ -1,58 +1,59 @@
-#include "stroka.h"
-#include <cstring>
-#include <iostream>
+#include "stroka.h"    
+#include <cstring>     
+#include <iostream>    
 
-using namespace std;
+using namespace std;  
 
 Stroka::Stroka(char *string) {
-    str = new char[strlen(string) + 1];
-    strcpy(str, string);
+    str = new char[strlen(string) + 1];  // Выделение памяти для строки
+    strcpy(str, string);                  // Копирование переданной строки в выделенную память
 }
 
 Stroka::Stroka() {
-    str = new char[1];
-    str[0] = '\0';
+    str = new char[1];  // Выделение памяти для пустой строки
+    str[0] = '\0';       // Установка первого символа как нулевой символ, обозначающий конец строки
 }
 
 Stroka::Stroka(const Stroka& s) {
-    str = new char[strlen(s.str) + 1];
-    strcpy(str, s.str);
+    str = new char[strlen(s.str) + 1];  // Выделение памяти для копии строки
+    strcpy(str, s.str);                  // Копирование строки из объекта s в текущий объект
 }
 
 Stroka& Stroka::operator=(const Stroka& s) {
     if (this != &s) {
-        delete[] str;
-        str = new char[strlen(s.str) + 1];
-        strcpy(str, s.str);
+        delete[] str;                    // Освобождение старой памяти
+        str = new char[strlen(s.str) + 1];  // Выделение новой памяти для строки
+        strcpy(str, s.str);                  // Копирование строки из объекта s в текущий объект
     }
-    return *this;
+    return *this;  // Возврат ссылки на текущий объект для поддержки цепочки присваиваний
 }
 
 Stroka& Stroka::operator+(const Stroka& s) {
-    char* temp = new char[strlen(str) + strlen(s.str) + 1];
-    strcpy(temp, str);
-    strcat(temp, s.str);
-    delete[] str;
-    str = temp;
-    return *this;
+    char* temp = new char[strlen(str) + strlen(s.str) + 1];  // Выделение временной памяти для конкатенации строк
+    strcpy(temp, str);                  // Копирование текущей строки во временную переменную
+    strcat(temp, s.str);                // Добавление к текущей строке строки из объекта s
+    delete[] str;                       // Освобождение старой памяти
+    str = temp;                         // Присвоение новой строки текущему объекту
+    return *this;                       // Возврат ссылки на текущий объект для поддержки цепочки операций
 }
 
 int Stroka::operator==(const Stroka& s) {
-    return strcmp(str, s.str) == 0;
+    return strcmp(str, s.str) == 0;     // Сравнение текущей строки с строкой из объекта s и возврат результата
 }
 
 int Stroka::dlina() {
-    return strlen(str);
+    return strlen(str);                 // Возвращение длины строки
 }
 
 void Stroka::vvod() {
-    cin >> str;
+    cin >> str;                         // Стандартный ввод строки из консоли
 }
 
 void Stroka::vyvod() {
-    cout << str;
+    cout << str;                        // Стандартный вывод строки в консоль
 }
 
 Stroka::~Stroka() {
-    delete[] str;
+    delete[] str;                       // Освобождение памяти, выделенной для строки при уничтожении объекта
 }
+
